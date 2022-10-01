41aeb2279d317288088115e7cb896ce720971c0f
minleejae <mmj9808@naver.com>
2022-10-01 23:43:42 +0900

    COMMENT: sns 액티비티 주석 추가

--- HaruDemo/app/src/main/java/com/example/harudemo/sns/SnsTestActivity.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/sns/SnsTestActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.harudemo.R
 
+//sns Activity
 class SnsTestActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sns_test)
+
 
     }
 }
