804ee6a7b2bf156578c48db6ce64b47bcd4aeabf
lkl4502 <lkl4502@naver.com>
2022-10-05 00:10:16 +0900

    FEATURE: 로그인 액티비티 추가

---
+++ HaruDemo/app/src/main/java/com/example/harudemo/auth/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.harudemo.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.harudemo.R
+
+
+//로그인 액티비티
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
