feb1069480ca4ddad3059e9c956b6f663d7adc53
minleejae <mmj9808@naver.com>
2022-10-07 04:30:32 +0900

    FEATURE: TEST용 API이용하여 무한스크롤 적용

--- HaruDemo/app/src/main/java/com/example/harudemo/fragments/SnsFragment.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/fragments/SnsFragment.kt
@@ -5,18 +5,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.harudemo.App
 import com.example.harudemo.R
 import com.example.harudemo.model.SnsPost
+import com.example.harudemo.retrofit.RetrofitManager
 import com.example.harudemo.sns.SnsAddPostActivity
 import com.example.harudemo.sns.SnsDirectMessageActivity
 import com.example.harudemo.sns.SnsFriendsActivity
 import com.example.harudemo.sns.recyclerview.SnsPostRecyclerViewAdapter
+import com.example.harudemo.utils.RESPONSE_STATUS
 import kotlinx.android.synthetic.main.fragment_sns.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class SnsFragment : Fragment() {
     private var totalCount = 0 // 전체 아이템 개수
@@ -118,61 +126,81 @@ class SnsFragment : Fragment() {
 
                 val layoutManager = sns_post_recycler_view.layoutManager
 
+                Log.d(TAG, "SnsFragment - onScrolled() called ${layoutManager?.itemCount}")
+
                 //뷰 상에서 마지막으로 보인 아이템 찾기
                 val lastVisibleItem = (layoutManager as LinearLayoutManager)
                     .findLastVisibleItemPosition()
 
-                //레이아웃 매니저가 지닌 전체 아이템 갯수와 현재 뷰에서 마지막 보이는 아이템 개수를 비교하여 API Call
-                if (layoutManager.itemCount <= lastVisibleItem + 2) {
+                // 스크롤이 끝에 도달했는지 확인
+                if (!sns_post_recycler_view.canScrollVertically(1)) {
                     ApiCallTest()
-                    snsPostRecyclerViewAdapter.notifyItemInserted(SnsPostList.size)
-
                 }
+                //레이아웃 매니저가 지닌 전체 아이템 갯수와 현재 뷰에서 마지막 보이는 아이템 개수를 비교하여 API Call
+//                if (layoutManager.itemCount <= lastVisibleItem + 2) {
+//
+//                }
             }
         })
     }
 
 
     fun ApiCallTest() {
-        this.SnsPostList.add(
-            SnsPost(
-                "유상록",
-                "게시물번호 " + (totalCount++).toString(),
-                "2022.10.04",
-                "none"
-            )
-        )
-        this.SnsPostList.add(
-            SnsPost(
-                "오홍석",
-                "게시물번호 " + (totalCount++).toString(),
-                "2022.10.04",
-                "none"
-            )
-        )
-        this.SnsPostList.add(
-            SnsPost(
-                "LMJ",
-                "게시물번호 " + (totalCount++).toString(),
-                "2022.10.04",
-                "none"
-            )
-        )
-        this.SnsPostList.add(
-            SnsPost(
-                "LMJ",
-                "게시물번호 " + (totalCount++).toString(),
-                "2022.10.04",
-                "none"
-            )
-        )
-        this.SnsPostList.add(
-            SnsPost(
-                "LMJ",
-                "게시물번호 " + (totalCount++).toString(),
-                "2022.10.04",
-                "none"
-            )
-        )
+//        this.SnsPostList.add(
+//            SnsPost(
+//                "유상록",
+//                "게시물번호 " + (totalCount++).toString(),
+//                "2022.10.04",
+//                "none"
+//            )
+//        )
+//        this.SnsPostList.add(
+//            SnsPost(
+//                "유상록",
+//                "게시물번호 " + (totalCount++).toString(),
+//                "2022.10.04",
+//                "none"
+//            )
+//        )
+//        this.SnsPostList.add(
+//            SnsPost(
+//                "유상록",
+//                "게시물번호 " + (totalCount++).toString(),
+//                "2022.10.04",
+//                "none"
+//            )
+//        )
+//        this.SnsPostList.add(
+//            SnsPost(
+//                "유상록",
+//                "게시물번호 " + (totalCount++).toString(),
+//                "2022.10.04",
+//                "none"
+//            )
+//        )
+//
+//        Log.d(TAG, "SnsFragment - ApiCallTest() called, 스크롤 리스너")
+
+
+        RetrofitManager.instance.getPosts(completion = { responseStatus, responseDataArrayList ->
+            Log.d(TAG, "SnsFragment - ApiCallTest() called ${responseStatus}")
+            when (responseStatus) {
+                //API 호출 성공
+
+                RESPONSE_STATUS.OKAY -> {
+                    responseDataArrayList!!.forEach {
+                        this.SnsPostList.add(it)
+                    }
+
+                    snsPostRecyclerViewAdapter.notifyItemInserted(SnsPostList.size)
+                }
+                RESPONSE_STATUS.FAIL -> {
+                    Toast.makeText(App.instance, "api 호출 에러입니다.", Toast.LENGTH_SHORT).show()
+                }
+                RESPONSE_STATUS.NO_CONTENT -> {
+                    Toast.makeText(App.instance, "더이상 게시물이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 }
---
+++ HaruDemo/app/src/main/java/com/example/harudemo/retrofit/RetrofitClient.kt
@@ -0,0 +1,112 @@
+package com.example.harudemo.retrofit
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import com.example.harudemo.App
+import com.example.harudemo.utils.API
+import com.example.harudemo.utils.Constants.TAG
+import com.example.harudemo.utils.isJsonArray
+import com.example.harudemo.utils.isJsonObject
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+import java.util.concurrent.TimeUnit
+
+
+object RetrofitClient {
+    private var retrofitClient : Retrofit? = null
+
+    fun getClient(baseUrl: String): Retrofit?{
+
+        //okhttp 인스턴스 생성
+        val client = OkHttpClient.Builder()
+
+        // 로그를 찍기 위해 로깅 인터셉터 추가
+        val loggingInterceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+//                Log.d(TAG, "RetrofitClient - log() called / message: $message")
+//
+//                when {
+//                    message.isJsonObject() ->
+//                        Log.d(TAG, JSONObject(message).toString(4))
+//                    message.isJsonArray() ->
+//                        Log.d(TAG, JSONObject(message).toString(4))
+//                    else -> {
+//                        try {
+//                            Log.d(TAG, JSONObject(message).toString(4))
+//                        } catch (e: Exception) {
+//                            Log.d(TAG, message)
+//                        }
+//                    }
+//                }
+            }
+        })
+
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        // 위에서 설정한 로깅 인터셉터를 okhttp 클라이언트에 추가한다.
+        client.addInterceptor(loggingInterceptor)
+
+
+        // 기본 파라미터 인터셉터 설정
+        val baseParameterInterceptor: Interceptor = (object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+
+                Log.d(TAG, "RetrofitClient - intercept() called abcdse")
+                // 오리지널 리퀘스트그
+                val originalRequest = chain.request()
+
+//                // 쿼리 파라미터 추가하기 추후 API KET 추가 용도
+//                val addedUrl =
+//                    originalRequest.url.newBuilder().addQueryParameter("client_id", API.API_KEY)
+//                        .build()
+//
+//                val finalRequest = originalRequest.newBuilder()
+//                    .url(addedUrl)
+//                    .method(originalRequest.method, originalRequest.body)
+//                    .build()
+//
+//                val response = chain.proceed(finalRequest)
+
+                val response = chain.proceed(originalRequest)
+                Log.d(TAG, "RetrofitClient - intercept() called response code : ${response.code}")
+
+                if(response.code != 200){
+                    Handler(Looper.getMainLooper()).post {
+                        Toast.makeText(App.instance, "${response.code} 에러 입니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                return response
+            }
+        })
+
+
+        // 위에서 설정한 기본 파라미터 인터셉터를 okhttp 클라이언트에 추가한다.
+        client.addInterceptor(baseParameterInterceptor)
+
+        // 커넥션 타임 아웃
+        client.connectTimeout(10, TimeUnit.SECONDS)
+        client.readTimeout(10, TimeUnit.SECONDS)
+        client.writeTimeout(10, TimeUnit.SECONDS)
+        client.retryOnConnectionFailure(true)
+
+        // 레트로핏 빌더를 통해 인스턴스 생성
+        if (retrofitClient == null) {
+            retrofitClient = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                // 위에서 설정한 클라이언트로 레트로핏 클라이언트를 설정한다.
+                .client(client.build())
+                .build()
+        }
+        return retrofitClient
+    }
+}
