31d47ec3a707a230f4e7836aed811c7fa6f94c5a
minleejae <mmj9808@naver.com>
2022-10-09 13:26:00 +0900

    FEATURE: backend server 기초 구조 생성

---
+++ server/src/entity/user.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+//필요한 데이터베이스 스키마 entity에 생성
+
+@Entity()
+export class User {
+  //id : auto Increment
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  nickName: string;
+}
