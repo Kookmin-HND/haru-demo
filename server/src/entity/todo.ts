cecd1f2b86076bc5ab895f679ba7d0a1871cfccb
cjeongmin <cjeongmin27@gmail.com>
2022-10-10 00:56:58 +0900

    FEATURE: Todo 스키마 생성

--- server/src/entity/todo.ts
+++ server/src/entity/todo.ts
@@ -3,17 +3,23 @@ import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 //필요한 데이터베이스 스키마 entity에 생성
 
 @Entity()
-export class User {
+export class Todo {
   //id : auto Increment
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  email: string;
-
-  @Column()
-  name: string;
+  writer: string;
 
   @Column()
   todoId: number;
+
+  @Column()
+  folder: string;
+
+  @Column()
+  content: string;
+
+  @Column({ type: Date })
+  createdAt: string;
 }
--- server/src/routes/todos.routes.ts
+++ server/src/routes/todos.routes.ts
@@ -7,7 +7,4 @@ export const path = "/todos";
 export const router = Router();
 
 //http://localhost:8000/api/todos
-router.get("/", async function (req: Request, res: Response) {
-  const users = await myDataSource.getRepository(User).findBy(req.body.email);
-  res.json(users);
-});
+router.get("/", async function (req: Request, res: Response) {});
