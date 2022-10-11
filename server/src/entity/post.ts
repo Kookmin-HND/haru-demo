d58b33c1aaf75a52c4ef860436afaffb13fd3102
minleejae <mmj9808@naver.com>
2022-10-11 22:37:19 +0900

    FEATURE: posts entity 추가

---
+++ server/src/entity/post.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Generated,
+} from "typeorm";
+
+//필요한 데이터베이스 스키마 entity에 생성
+
+@Entity()
+export class Post {
+  //id : auto Increment
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  writer: string;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
