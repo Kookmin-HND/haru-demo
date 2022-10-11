f9e339067ce5ad9d8601c4c0d477f9304d711a38
minleejae <mmj9808@naver.com>
2022-10-11 22:37:58 +0900

    FEATURE: 게시물 입력, 최신 게시물 불러오기 api 추가

--- server/src/routes/posts.routes.ts
+++ server/src/routes/posts.routes.ts
@@ -1,35 +1,42 @@
 import { Router } from "express";
 import express, { Request, Response, NextFunction } from "express";
 import myDataSource from "../app-data-source";
-import { User } from "../entity/user";
+import { Post } from "../entity/post";
+import { MoreThan } from "typeorm";
 
 export const path = "/posts";
 export const router = Router();
 
 //postid 이후 최근 게시물 50개 정보 sns fragment에서 보여지는 정보
-router.get(
-  "/recent/:postId",
-  (req: Request, res: Response, next: NextFunction) => {
-    res.send("test!!");
-  }
-);
+router.get("/recent/:postId", async function (req: Request, res: Response) {
+  //마지막으로 읽은 post Id
+  const readedPostId = Number(req.params.postId);
+  const results = await myDataSource.getRepository(Post).find({
+    where: { id: MoreThan(readedPostId) },
+    take: 50,
+    order: { id: "DESC" },
+  });
+  res.json(results);
+});
 
 //게시물 하나의 정보 조회
-router.post("/", async function (req: Request, res: Response) {
+router.get("/:postId", async function (req: Request, res: Response) {
   res.json("");
 });
 
 //게시물 입력
 router.post("/", async function (req: Request, res: Response) {
-  res.json("");
+  const post = myDataSource.getRepository(Post).create(req.body);
+  const results = await myDataSource.getRepository(Post).save(post);
+  res.json(results);
 });
 
 //게시물 삭제요청
-router.delete("/", async function (req: Request, res: Response) {
+router.delete("/:postId", async function (req: Request, res: Response) {
   res.json("");
 });
 
 //게시물 수정
-router.post("/", async function (req: Request, res: Response) {
+router.patch("/:postId", async function (req: Request, res: Response) {
   res.json("");
 });
