8474aca45474ee29db696f12084a142e50727968
lkl4502 <lkl4502@naver.com>
2022-10-12 18:06:46 +0900

    FEATURE : User get 요청 기능 구현

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -2,11 +2,31 @@ import { Router } from "express";
 import express, { Request, Response, NextFunction } from "express";
 import myDataSource from "../app-data-source";
 import { User } from "../entity/user";
+// import { Equal } from "typeorm";
 
 export const path = "/users";
 export const router = Router();
 
+interface UserParams {
+  email: string;
+}
+
 //http://localhost:8000/api/users/ ~~
+
+// user 정보조회
+router.get("/:email", async function (req: Request<UserParams>, res: Response) {
+  console.log("get : ", req.params.email);
+
+  try {
+    const result = await myDataSource
+      .getRepository(User)
+      .findOneByOrFail({ email: req.params.email });
+    return res.send(result);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json("No user found");
+  }
+});
 
 // user 로그인 기능
 router.post("/", async function (req: Request, res: Response) {});
@@ -16,9 +36,6 @@ router.post("/", async function (req: Request, res: Response) {});
 
 // user 정보수정
 router.patch("/", async function (req: Request, res: Response) {});
-
-// user 정보조회
-router.get("/", async function (req: Request, res: Response) {});
 
 // user 회원탈퇴
 router.delete("/", async function (req: Request, res: Response) {});
