1a6d491abc7b4d2a0fb401117476330ba9fdc02f
minleejae <mmj9808@naver.com>
2022-10-13 21:43:36 +0900

    FEATURE: 파라미터 타입 명시

--- server/src/routes/posts.routes.ts
+++ server/src/routes/posts.routes.ts
@@ -14,20 +14,23 @@ interface PostParams {
 // type RecentPostResponseBody = Post[];
 
 //postid 이후 최근 게시물 50개 정보 sns fragment에서 보여지는 정보
-router.get("/recent/:postId", async (req: Request, res: Response) => {
-  //마지막으로 읽은 post Id
-  const readedPostId = Number(req.params.postId);
-  const result = await myDataSource.getRepository(Post).find({
-    where: { id: MoreThan(readedPostId) },
-    take: 50,
-    order: { id: "DESC" },
-  });
+router.get(
+  "/recent/:postId",
+  async (req: Request<PostParams>, res: Response) => {
+    //마지막으로 읽은 post Id
+    const readedPostId = Number(req.params.postId);
+    const result = await myDataSource.getRepository(Post).find({
+      where: { id: MoreThan(readedPostId) },
+      take: 50,
+      order: { id: "DESC" },
+    });
 
-  if (!result.length)
-    return res.status(400).send("게시물이 존재하지 않습니다.");
+    if (!result.length)
+      return res.status(400).send("게시물이 존재하지 않습니다.");
 
-  return res.json(result);
-});
+    return res.json(result);
+  }
+);
 
 //게시물 하나의 정보 조회
 router.get("/:postId", async (req: Request<PostParams>, res: Response) => {
@@ -52,30 +55,36 @@ router.post("/", async function (req: Request, res: Response) {
 });
 
 //게시물 삭제요청
-router.delete("/:postId", async function (req: Request, res: Response) {
-  const postId = Number(req.params.postId);
-  const result = await myDataSource.getRepository(Post).delete(postId);
+router.delete(
+  "/:postId",
+  async function (req: Request<PostParams>, res: Response) {
+    const postId = Number(req.params.postId);
+    const result = await myDataSource.getRepository(Post).delete(postId);
 
-  //affected : 0 실패, affected : 1 성공
-  if (!result.affected)
-    return res.status(400).send("게시물 삭제에 실패했습니다.");
+    //affected : 0 실패, affected : 1 성공
+    if (!result.affected)
+      return res.status(400).send("게시물 삭제에 실패했습니다.");
 
-  return res.json(result);
-});
+    return res.json(result);
+  }
+);
 
 //게시물 수정
-router.patch("/:postId", async function (req: Request, res: Response) {
-  const postId = Number(req.params.postId);
-  const content: string = req.body.content;
+router.patch(
+  "/:postId",
+  async function (req: Request<PostParams>, res: Response) {
+    const postId = Number(req.params.postId);
+    const content: string = req.body.content;
 
-  // id가 postId에 해당하는 게시글의 content 수정
-  const result = await myDataSource
-    .getRepository(Post)
-    .update({ id: postId }, { content });
+    // id가 postId에 해당하는 게시글의 content 수정
+    const result = await myDataSource
+      .getRepository(Post)
+      .update({ id: postId }, { content });
 
-  //affected : 0 실패, affected : 1 성공
-  if (!result.affected)
-    return res.status(400).send("게시물 수정에 실패했습니다.");
+    //affected : 0 실패, affected : 1 성공
+    if (!result.affected)
+      return res.status(400).send("게시물 수정에 실패했습니다.");
 
-  return res.json(result);
-});
+    return res.json(result);
+  }
+);
