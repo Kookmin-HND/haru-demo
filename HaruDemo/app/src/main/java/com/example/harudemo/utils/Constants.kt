7cb978e3f4c09c5dd8e3215844f469f59608e7ab
cjeongmin <cjeongmin27@gmail.com>
2022-11-02 10:13:27 +0900

    FIX: xml 파일 추가

--- HaruDemo/app/src/main/java/com/example/harudemo/utils/Constants.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/utils/Constants.kt
@@ -1,17 +1,18 @@
 package com.example.harudemo.utils
 
-object Constants{
-    const val TAG : String = "로그"
+object Constants {
+    const val TAG: String = "로그"
 }
 
 //API 호출 결과 enum
-enum class RESPONSE_STATUS{
+enum class RESPONSE_STATUS {
     OKAY, FAIL, NO_CONTENT
 }
 
 //테스트용 API
-object API{
-    const val BASE_URL : String = "https://jsonplaceholder.typicode.com/"
-    const val API_KEY : String = ""
-    const val POSTS : String = "posts"
+object API {
+    const val BASE_URL: String = "https://jsonplaceholder.typicode.com/"
+    const val API_KEY: String = ""
+    const val POSTS: String = "posts"
+    const val TODOS: String = "todos"
 }
