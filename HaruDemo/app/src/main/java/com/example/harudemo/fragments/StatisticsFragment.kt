71bcb0012a9bb2027d6aa87acf57a4e9357ec169
ablecp <tasong12@kookmin.ac.kr>
2022-11-03 20:29:53 +0900

    FEATURE:글작성 댓글달기 기능 구현 ui 개선

--- HaruDemo/app/src/main/java/com/example/harudemo/fragments/StatisticsFragment.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/fragments/StatisticsFragment.kt
@@ -12,16 +12,15 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.harudemo.R
 import com.example.harudemo.databinding.FragmentStatisticsBinding
-import com.example.harudemo.databinding.FragmentTodoBinding
 import com.example.harudemo.grass.grassAdapter
 import kotlinx.android.synthetic.main.fragment_statistics.*
 import java.util.*
 
-class StatisticsFragment: Fragment() {
-    companion object{
-        const val TAG : String = "로그"
+class StatisticsFragment : Fragment() {
+    companion object {
+        const val TAG: String = "로그"
 
-        fun newInstance() : StatisticsFragment {
+        fun newInstance(): StatisticsFragment {
             return StatisticsFragment()
         }
     }
@@ -30,7 +29,7 @@ class StatisticsFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "Ranking - on Create() called" )
+        Log.d(TAG, "Ranking - on Create() called")
     }
 
 
@@ -48,6 +47,14 @@ class StatisticsFragment: Fragment() {
     ): View? {
         Log.d(TAG, "Ranking - onCreateView() called")
         val view = inflater.inflate(R.layout.fragment_statistics, container, false)
+        //프로그래스바 값 수정
+        binding?.todayBar?.progress = 70 //오늘 프로그래스 바 퍼센트
+        binding?.todayText?.text = "70%" //오늘 퍼센트 텍스트
+        binding?.weekBar?.progress = 60 //이번주 프로그래스 바 퍼센트
+        binding?.weekText?.text = "60%" //이번주 퍼센트 텍스트
+        binding?.monthBar?.progress = 15 //이번달 프로그래스 바 퍼센트
+        binding?.monthText?.text = "15%" //이번달 퍼센트 텍스트
+        //프로그래스바 값 수정
 
         return view
     }
@@ -66,14 +73,6 @@ class StatisticsFragment: Fragment() {
         val tempMonth = calendar.get(Calendar.MONTH)
         val dayListManager = GridLayoutManager(this.context, 7)
         val dayListAdapter = grassAdapter(tempMonth)
-        //프로그래스바 값 수정
-        binding?.todayBar?.progress = 70 //오늘 프로그래스 바 퍼센트
-        binding?.todayText?.text = "70%" //오늘 퍼센트 텍스트
-        binding?.weekBar?.progress = 60 //이번주 프로그래스 바 퍼센트
-        binding?.weekText?.text = "60%" //이번주 퍼센트 텍스트
-        binding?.monthBar?.progress = 45 //이번달 프로그래스 바 퍼센트
-        binding?.monthText?.text = "45%" //이번달 퍼센트 텍스트
-        //프로그래스바 값 수정
 
         statistic_vp.apply {
             layoutManager = dayListManager
