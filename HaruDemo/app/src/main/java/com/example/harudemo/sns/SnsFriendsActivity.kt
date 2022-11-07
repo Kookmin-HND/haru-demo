6503b10c8ab446cc87b8f31c2be19fcf24454455
ablecp <tasong12@kookmin.ac.kr>
2022-11-07 14:35:18 +0900

    친구목록 캘린더 디자인 수정

--- HaruDemo/app/src/main/java/com/example/harudemo/sns/SnsFriendsActivity.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/sns/SnsFriendsActivity.kt
@@ -2,14 +2,14 @@ package com.example.harudemo.sns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 import com.example.harudemo.R
+import com.example.harudemo.databinding.ActivitySnsFriendsBinding
 
 //SNS 프래그먼트에서 친구목록을 확인할 수 있는 액티비티
 class SnsFriendsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sns_friends)
-
     }
 }
