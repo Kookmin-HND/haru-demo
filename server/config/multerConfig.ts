1776ccca40ef71242935c74de09d3295eb62461f
minleejae <mmj9808@naver.com>
2022-11-11 22:15:56 +0900

    FEATURE: 글쓰기시 사진 보낼 수 있는 API 추가

--- server/config/multerConfig.ts
+++ server/config/multerConfig.ts
@@ -1,17 +1,43 @@
-import { Request } from "express";
-import multer, { FileFilterCallback } from "multer";
+import aws from "aws-sdk";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+import { v4 } from "uuid";
 
-type FileNameCallback = (error: Error | null, filename: string) => void;
-
-export const multerConfig = {
-  storage: multer.diskStorage({
-    destination: "uploads/",
-    filename: function (
-      req: Request,
-      file: Express.Multer.File,
-      cb: FileNameCallback
-    ) {
-      cb(null, file.originalname);
-    },
-  }),
+const multerFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb({ msg: "image 파일만 업로드 가능합니다." }, false);
+  }
 };
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY as string,
+    secretAccessKey: process.env.S3_SECRET_KEY as string,
+  },
+  region: "ap-northeast-2",
+});
+
+//S3 연동 config 지정
+export const awsUpload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.BUCKET_NAME as string,
+    metadata: function (req: any, file: any, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req: any, file, cb) {
+      //원본파일의 파일 타입을 알아내서 파일 지정
+      const fileType = "." + file.mimetype.split("/")[1];
+      const fileDirectory = "post/";
+      const fileName = file.fieldname + v4();
+      const fileLocation = fileDirectory + fileName + fileType;
+      cb(null, fileLocation);
+    },
+    acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+  }),
+  fileFilter: multerFilter,
+});
--- server/src/routes/posts.routes.ts
+++ server/src/routes/posts.routes.ts
@@ -3,8 +3,8 @@ import express, { Request, Response, NextFunction } from "express";
 import myDataSource from "../app-data-source";
 import { Post } from "../entity/post";
 import { LessThan, MoreThan } from "typeorm";
-import multer, { FileFilterCallback } from "multer";
-import { multerConfig } from "../../config/multerConfig";
+import { awsUpload } from "../../config/multerConfig";
+import { ImageFile } from "../entity/imageFile";
 
 export const path = "/posts";
 export const router = Router();
@@ -13,34 +13,31 @@ interface PostParams {
   postId: number;
 }
 
-const upload = multer(multerConfig); //dest : 저장 위치
-
 // multer test
-router.post("/upload", upload.single("img"), (req: any, res: any) => {
-  const file = req.files;
-  console.log(file);
-
-  res.json(req.file);
+router.post("/upload", awsUpload.array("img"), async (req: any, res: any) => {
+  const locationList: Array<String> = [];
+  const filesList: Array<any> = req.files;
+  filesList.forEach((file) => {
+    locationList.push(file.location);
+  });
+  // 업로드한 이미지 파일 url 전달
+  res.json({ locationList });
 });
 
 //postid 이후 최근 게시물 50개 정보 sns fragment에서 보여지는 정보
-router.get(
-  "/recent/:postId",
-  async (req: Request<PostParams>, res: Response) => {
-    //마지막으로 읽은 postId를 바탕으로 이후 게시물 50개를 가져온다
-    const readedPostId = Number(req.params.postId);
-    const result = await myDataSource.getRepository(Post).find({
-      where: { id: LessThan(readedPostId) },
-      take: 50,
-      order: { id: "DESC" },
-    });
+router.get("/recent/:postId", async (req: Request<PostParams>, res: Response) => {
+  //마지막으로 읽은 postId를 바탕으로 이후 게시물 50개를 가져온다
+  const readedPostId = Number(req.params.postId);
+  const result = await myDataSource.getRepository(Post).find({
+    where: { id: LessThan(readedPostId) },
+    take: 50,
+    order: { id: "DESC" },
+  });
 
-    if (!result.length)
-      return res.status(400).send("게시물이 존재하지 않습니다.");
+  if (!result.length) return res.status(400).send("게시물이 존재하지 않습니다.");
 
-    return res.json(result);
-  }
-);
+  return res.json(result);
+});
 
 //게시물 하나의 정보 조회
 router.get("/:postId", async (req: Request<PostParams>, res: Response) => {
@@ -58,17 +55,32 @@ router.get("/:postId", async (req: Request<PostParams>, res: Response) => {
 });
 
 //게시물 입력
-router.post("/:email", async (req: Request, res: Response) => {
+router.post("/:email", awsUpload.array("img"), async (req: any, res: Response) => {
   //req.body에 있는 정보를 바탕으로 새로운 게시물 데이터를 생성한다.
 
-  console.log("hererere@@@@@@@@");
-  console.log(req);
+  console.log(req.body);
   const writer = req.params.email;
 
+  //이미지 S3에 저장한 url
+  const locationList: Array<string> = [];
+  const filesList: Array<any> = req.files;
+  filesList.forEach((file) => {
+    locationList.push(file.location);
+  });
+
+  const imageFileList: Array<any> = [];
+
+  locationList.forEach((item) => {
+    console.log(item);
+    imageFileList.push(myDataSource.getRepository(ImageFile).create({ post: req.body.postId, url: item }));
+  });
+
   const post = myDataSource.getRepository(Post).create({ ...req.body, writer });
+  await myDataSource.getRepository(ImageFile).save(imageFileList);
   const result = await myDataSource.getRepository(Post).save(post);
 
-  return res.json(result);
+  // 업로드한 이미지 파일 url 전달
+  return res.json({ ...result, locationList });
 });
 
 //게시물 삭제요청
@@ -77,8 +89,7 @@ router.delete("/:postId", async (req: Request<PostParams>, res: Response) => {
   const result = await myDataSource.getRepository(Post).delete(postId);
 
   //affected : 0 실패, affected : 1 성공
-  if (!result.affected)
-    return res.status(400).send("게시물 삭제에 실패했습니다.");
+  if (!result.affected) return res.status(400).send("게시물 삭제에 실패했습니다.");
 
   return res.json(result);
 });
@@ -89,13 +100,10 @@ router.patch("/:postId", async (req: Request<PostParams>, res: Response) => {
   const content: string = req.body.content;
 
   // id가 postId에 해당하는 게시글의 content 수정
-  const result = await myDataSource
-    .getRepository(Post)
-    .update({ id: postId }, { content });
+  const result = await myDataSource.getRepository(Post).update({ id: postId }, { content });
 
   //affected : 0 실패, affected : 1 성공
-  if (!result.affected)
-    return res.status(400).send("게시물 수정에 실패했습니다.");
+  if (!result.affected) return res.status(400).send("게시물 수정에 실패했습니다.");
 
   return res.json(result);
 });
