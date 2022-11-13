240be976f0e340f86bb80e0b7456e44308fc6362
minleejae <mmj9808@naver.com>
2022-11-13 20:17:36 +0900

    FEATURE: sns post 미리보기 이미지 개수에 따라 다르게 보여주기

--- HaruDemo/app/src/main/java/com/example/harudemo/sns/recyclerview/SnsPostItemViewHolder.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/sns/recyclerview/SnsPostItemViewHolder.kt
@@ -1,6 +1,5 @@
 package com.example.harudemo.sns.recyclerview
 
-import android.text.method.TextKeyListener.clear
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -10,32 +9,78 @@ import com.example.harudemo.model.SnsPost
 import kotlinx.android.synthetic.main.layout_sns_post_item.view.*
 
 //Sns 게시물 리사이클러뷰를 위한 뷰홀더
-class SnsPostItemViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView) {
+class SnsPostItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val snsPostWriterPhoto = itemView.sns_post_writer_photo
     private val snsPostWriterName = itemView.sns_post_writer_name
     private val snsPostCreatedAt = itemView.sns_post_created_at
     private val snsPostContent = itemView.sns_post_content
-    private val snsPostPreviewImage = itemView.sns_post_preview_image
+    private val snsPostPreviewImageOneCardView = itemView.sns_post_preview_image_one_cardView
+    private val snsPostPreviewImageTwoCardView = itemView.sns_post_preview_image_two_cardView
+    private val snsPostPreviewImageThreeCardView = itemView.sns_post_preview_image_three_cardView
 
+    private val snsPostPreviewImageOne = itemView.sns_post_preview_image_one
+    private val snsPostPreviewImageTwoFirst = itemView.sns_post_preview_image_two_first
+    private val snsPostPreviewImageTwoSecond = itemView.sns_post_preview_image_two_second
+    private val snsPostPreviewImageThreeFirst = itemView.sns_post_preview_image_three_first
+    private val snsPostPreviewImageThreeSecond = itemView.sns_post_preview_image_three_second
+    private val snsPostPreviewImageThreeThird = itemView.sns_post_preview_image_three_third
 
-    fun bindWidthView(snsPostItem : SnsPost){
+    fun bindWidthView(snsPostItem: SnsPost) {
         snsPostWriterName.text = snsPostItem.writer
         snsPostCreatedAt.text = snsPostItem.createdAt
         snsPostContent.text = snsPostItem.content
 
+        //이미지의 개수에 따라 미리보기 다르게 표현
+        if (snsPostItem.postImageList.isEmpty()) {//이미지가 없는 게시글인경우
+            snsPostPreviewImageOneCardView.visibility = View.GONE
+            snsPostPreviewImageTwoCardView.visibility = View.GONE
+            snsPostPreviewImageThreeCardView.visibility = View.GONE
+        } else if (snsPostItem.postImageList.size == 1) {
+            snsPostPreviewImageOneCardView.visibility = View.VISIBLE
+            snsPostPreviewImageTwoCardView.visibility = View.GONE
+            snsPostPreviewImageThreeCardView.visibility = View.GONE
 
-        if(snsPostItem.postImageList.isNotEmpty()){
-            snsPostPreviewImage.visibility = View.VISIBLE
             Glide.with(App.instance)
                 .load(snsPostItem.postImageList[0])
                 .placeholder(R.drawable.ic_baseline_account_circle_24)
-                .into(snsPostPreviewImage)
-        }
-        else{//이미지가 없는 게시글인경우
-            snsPostPreviewImage.visibility = View.GONE
-        }
+                .into(snsPostPreviewImageOne)
+        } else if (snsPostItem.postImageList.size == 2) {
+            snsPostPreviewImageOneCardView.visibility = View.GONE
+            snsPostPreviewImageTwoCardView.visibility = View.VISIBLE
+            snsPostPreviewImageThreeCardView.visibility = View.GONE
+
+            Glide.with(App.instance)
+                .load(snsPostItem.postImageList[0])
+                .placeholder(R.drawable.ic_baseline_account_circle_24)
+                .into(snsPostPreviewImageTwoFirst)
+
+            Glide.with(App.instance)
+                .load(snsPostItem.postImageList[1])
+                .placeholder(R.drawable.ic_baseline_account_circle_24)
+                .into(snsPostPreviewImageTwoSecond)
 
 
+        } else if (snsPostItem.postImageList.size >= 3) {
+            snsPostPreviewImageOneCardView.visibility = View.GONE
+            snsPostPreviewImageTwoCardView.visibility = View.GONE
+            snsPostPreviewImageThreeCardView.visibility = View.VISIBLE
+
+
+            Glide.with(App.instance)
+                .load(snsPostItem.postImageList[0])
+                .placeholder(R.drawable.ic_baseline_account_circle_24)
+                .into(snsPostPreviewImageThreeFirst)
+
+            Glide.with(App.instance)
+                .load(snsPostItem.postImageList[1])
+                .placeholder(R.drawable.ic_baseline_account_circle_24)
+                .into(snsPostPreviewImageThreeSecond)
+
+            Glide.with(App.instance)
+                .load(snsPostItem.postImageList[2])
+                .placeholder(R.drawable.ic_baseline_account_circle_24)
+                .into(snsPostPreviewImageThreeThird)
+        }
         //프로필 이미지 생성
         Glide.with(App.instance)
             .load(snsPostItem.writerPhoto)
