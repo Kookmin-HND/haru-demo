9039f76d23ba65df0c1e50fc735f5f24285d8a83
lkl4502 <lkl4502@naver.com>
2022-11-22 18:23:15 +0900

    FIX : 카카오톡 로그인시 버벅거리는 현상 개선

--- HaruDemo/app/src/main/java/com/example/harudemo/auth/LoginActivity.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/auth/LoginActivity.kt
@@ -126,7 +126,7 @@ class LoginActivity : AppCompatActivity() {
             else if (token != null) {
                 CustomToast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                startActivity(intent)
                 finish()
             }
         }
