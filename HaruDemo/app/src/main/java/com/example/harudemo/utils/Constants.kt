028428e645af0e9edaf39d37de389ef3b9c42669
cjeongmin <cjeongmin27@gmail.com>
2022-11-22 20:43:47 +0900

    FIX: BASE URL 변경

--- HaruDemo/app/src/main/java/com/example/harudemo/utils/Constants.kt
+++ HaruDemo/app/src/main/java/com/example/harudemo/utils/Constants.kt
@@ -8,14 +8,16 @@ object Constants {
 enum class RESPONSE_STATUS {
     OKAY, FAIL, NO_CONTENT
 }
+
 //10.0.0.2
 // kmu5g 10.30.113.12
 // 106 - 5g 192.168.0.48
 object API {
 
     //const val BASE_URL: String = "http://192.168.0.7:8000/api/"
-//    const val BASE_URL: String = "http://10.0.2.2:8000/api/"
-    const val BASE_URL: String = "http://10.30.119.142:8000/api/"
+    const val BASE_URL: String = "http://10.0.2.2:8000/api/"
+
+    //    const val BASE_URL: String = "http://10.30.119.142:8000/api/"
     const val API_KEY: String = ""
     const val RECENT_POSTS: String = "posts/recent/{postId}"
     const val POSTS: String = "posts"
